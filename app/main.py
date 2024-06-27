6bfbb2b5ed7a36150ed8fde40ec292c77b60973d
nouryasmine <bekhtinourelhoudayasmine@gmail.com>
2024-06-27 10:23:25 +0100

    Initial commit

---
+++ app/main.py
@@ -0,0 +1,95 @@
+import base64
+from io import BytesIO
+import logging
+from typing import List
+from fastapi import FastAPI, HTTPException, Response
+from fastapi.responses import JSONResponse
+from matplotlib.pyplot import draw
+from pydantic import BaseModel
+from app.model.model import predict_dipolemoment, predict_gap, predict_homo, generate_molecules, predict_lumo, predict_zpve, smilesToMol, visualize_molecules
+import py3Dmol
+#from app.model.model import model
+from fastapi.middleware.cors import CORSMiddleware
+import torch
+from rdkit import Chem
+from rdkit.Chem import AllChem
+from rdkit.Chem import Draw
+#uvicorn app.main:app 
+app= FastAPI()
+origins =[
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+
+]
+
+app.add_middleware(
+    CORSMiddleware,
+     allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+class Molecule(BaseModel):
+    molecule:str
+
+class PredictionOut(BaseModel):
+    gap:float
+    homo:float
+    lumo:float
+    zpve:float
+    dipolemoment:float
+
+class MoleculeRequest(BaseModel):
+    num_molecules: int = 10
+
+class MoleculeList(BaseModel):
+    moleculeslist: List[str]
+#C1=CC=CC=C1
+
+@app.get("/")
+def home():
+    return {"health_check":"ok"}
+
+
+@app.post("/predict", response_model=PredictionOut)
+def predict(payload: Molecule):
+    dipolemoment=predict_dipolemoment(payload.molecule)
+    lumo = predict_lumo(payload.molecule)
+    homo = predict_homo(payload.molecule)
+    gap = lumo-homo
+    zpve = predict_zpve(payload.molecule)
+    
+    return {"dipolemoment":dipolemoment, "lumo":lumo, "homo":homo, "gap": gap, "zpve":zpve}
+
+@app.post("/generate_molecules")
+def generate_molecules_endpoint(request: MoleculeRequest):
+    molecules = generate_molecules()
+    return {"molecules": molecules}
+
+@app.post("/visualize_molecules")
+async def visualize_mols(request: MoleculeList):
+    try:
+        logging.info(f"Received request: {request.moleculeslist}")
+        images = visualize_molecules(request.moleculeslist)
+        logging.info("Generated image successfully")
+        return Response(content=images, media_type="image/png")
+    except Exception as e:
+        logging.error(f"Error processing request: {e}")
+        raise HTTPException(status_code=500, detail=str(e))
+    
+@app.post("/visualize3d")
+async def visualize3d(request:MoleculeList):
+    try:
+        processed_molecules = smilesToMol(request.moleculeslist)
+        
+        # Generate 3D visualization for each molecule
+        pdb_strings = []
+        for mol in processed_molecules:
+            pdb_str = Chem.MolToPDBBlock(mol)
+            pdb_strings.append(pdb_str)
+            
+        return {"pdb_strings": pdb_strings}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
---
+++ app/model/model.py
@@ -0,0 +1,191 @@
+import base64
+from io import BytesIO
+from typing import List
+from fastapi import HTTPException, Response
+import torch
+from pathlib import Path
+from torch_geometric.data import Data
+from rdkit import Chem
+from rdkit.Chem import AllChem
+import numpy as np
+import torch
+import torch.nn.functional as F
+from torch_geometric.loader import DataLoader
+from torch_geometric.nn import GATConv, global_mean_pool
+from torch_geometric.datasets import QM9
+from torch_geometric.transforms import Compose, NormalizeFeatures
+from app.model.utils import load_from_file, sample
+import torch
+import torch.nn.functional as F
+from rdkit import Chem
+from rdkit.Chem.rdmolops import GetAdjacencyMatrix
+from rdkit.Chem import Draw
+
+BASE_DIR = Path(__file__).resolve(strict=True).parent
+
+
+
+
+class GAT(torch.nn.Module):
+    def __init__(self, num_node_features):
+        super(GAT, self).__init__()
+        self.conv1 = GATConv(num_node_features, 128, heads=8, dropout=0.7)
+        self.conv2 = GATConv(128 * 8, 64, heads=8, dropout=0.7)
+        self.conv3 = GATConv(64 * 8, 32, heads=4, dropout=0.7)
+        self.out = torch.nn.Linear(32 * 4, 1)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = F.dropout(x, p=0.7, training=self.training)
+        x = F.leaky_relu(self.conv1(x, edge_index))
+        x = F.dropout(x, p=0.7, training=self.training)
+        x = F.leaky_relu(self.conv2(x, edge_index))
+        x = F.dropout(x, p=0.7, training=self.training)
+        x = F.leaky_relu(self.conv3(x, edge_index))
+        x = global_mean_pool(x, data.batch)
+        x = self.out(x)
+        return x
+
+num_node_features = 11  # Number of features per node in the QM9 dataset
+model = GAT(num_node_features)
+
+#with open (f"{BASE_DIR}/mymodel.pth","rb") as f:
+model.load_state_dict(torch.load(r'C:\Users\yasmine\Desktop\my models\app\model\model_state.pth'))
+model.eval()
+
+model2=  GAT(num_node_features)
+model2.load_state_dict(torch.load(r'C:\Users\yasmine\Desktop\my models\app\model\model_state_homo.pth'))
+model2.eval()
+
+pretrained_rnn_model = load_from_file(r'C:\Users\yasmine\Desktop\my models\app\model\pretrained.rnn.pth')
+
+model3=  GAT(num_node_features)
+model3.load_state_dict(torch.load(r'C:\Users\yasmine\Desktop\my models\app\model\model_state_dipolemoment.pth'))
+model3.eval()
+
+model4=  GAT(num_node_features)
+model4.load_state_dict(torch.load(r'C:\Users\yasmine\Desktop\my models\app\model\model_state_lumo.pth'))
+model4.eval()
+
+model5=  GAT(num_node_features)
+model5.load_state_dict(torch.load(r'C:\Users\yasmine\Desktop\my models\app\model\model_state_zpve.pth'))
+model5.eval()
+
+
+def atom_features(atom):
+    features = [
+        float(atom.GetAtomicNum()),                     # Atomic number
+        float(atom.GetChiralTag()),                     # Chirality (as a float)
+        float(atom.GetDegree()),                        # Degree (number of bonds)
+        float(atom.GetFormalCharge()),                  # Formal charge
+        float(atom.GetTotalNumHs()),                    # Number of bonded hydrogen atoms
+        float(atom.GetHybridization()),                 # Hybridization state (as float)
+        float(atom.GetIsAromatic()),                    # Aromaticity (as float)
+        float(atom.GetTotalNumHs()),                    # Total number of implicit hydrogens
+        float(atom.GetTotalDegree()),                   # Total degree
+        float(atom.GetImplicitValence()),               # Implicit valence
+        float(atom.GetExplicitValence())                # Explicit valence
+    ]
+    return features
+
+# Convert a SMILES string to a PyTorch Geometric Data object
+def smiles_to_graph(smiles : str) -> Data:
+    mol = Chem.MolFromSmiles(smiles)
+    if mol is None:
+        print(f"Error: Could not parse SMILES string: {smiles}")
+        return None
+
+
+
+    # Get adjacency matrix
+    adj = GetAdjacencyMatrix(mol)
+    
+    # Get atom features
+    atom_features_list = [atom_features(atom) for atom in mol.GetAtoms()]
+    
+    # Create edge index
+    edge_index = torch.tensor(np.array(np.nonzero(adj)), dtype=torch.long)
+    
+    if edge_index.size(1) == 0:
+        print(f"Error: No edges found in the adjacency matrix for SMILES string: {smiles}")
+        return None
+
+    
+    # Create node features
+    x = torch.tensor(np.array(atom_features_list), dtype=torch.float)
+    
+    # Create data object
+    data = Data(x=x, edge_index=edge_index)
+    return data
+
+
+def convert_to_smiles(molecules):
+    smiles_list = []
+    for mol in molecules:
+        smiles = Chem.MolToSmiles(mol)
+        smiles_list.append(smiles)
+    return smiles_list
+
+def predict_gap(molecule : str )-> float:
+    m=smiles_to_graph(molecule)
+    pred= model(m)
+    return pred.item()
+
+
+def predict_homo(molecule : str )-> float:
+    m=smiles_to_graph(molecule)
+    pred1= model2(m)
+    return pred1.item()
+
+def predict_lumo(molecule : str )-> float:
+    m=smiles_to_graph(molecule)
+    pred3= model4(m)
+    return pred3.item()
+
+
+def predict_dipolemoment(molecule : str )-> float:
+    m=smiles_to_graph(molecule)
+    pred2= model3(m)
+    return pred2.item()
+
+def predict_zpve(molecule : str )-> float:
+    m=smiles_to_graph(molecule)
+    pred4= model5(m)
+    return pred4.item()
+
+
+def generate_molecules():
+    generated_molecules = []
+    while len(generated_molecules) != 10:
+        # Generate token sequences
+        sequences, nlls = sample(model=pretrained_rnn_model)
+        
+        # Convert the token sequences into SMILES
+        smiles = pretrained_rnn_model.tokenizer.untokenize(pretrained_rnn_model.vocabulary.decode(sequences[0].numpy()))
+        
+        # Transform the generated SMILES into RDKit Mol objects
+        # The Mol object is "None" if the SMILES cannot be parsed by RDKit
+        mol = Chem.MolFromSmiles(smiles)
+        if mol is not None:
+            # At this point, the Mol is valid so let's keep track of it
+            generated_molecules.append(mol)
+        smiles_list= convert_to_smiles(generated_molecules)
+    return smiles_list
+
+def visualize_molecules(smile_list: List[str]):
+    generated_mols = [Chem.MolFromSmiles(smiles) for smiles in smile_list]
+    img = Draw.MolsToGridImage(generated_mols, molsPerRow=5)
+    buf = BytesIO()
+    img.save(buf, format='PNG')
+    byte_im = buf.getvalue()
+    return byte_im
+
+def smilesToMol(smiles_list: List[str]):
+    processed_molecules = []
+    for smiles in smiles_list:
+        mol = Chem.MolFromSmiles(smiles)
+        mol = Chem.AddHs(mol)
+        AllChem.EmbedMolecule(mol)
+        AllChem.UFFOptimizeMolecule(mol)
+        processed_molecules.append(mol)
+    return processed_molecules
